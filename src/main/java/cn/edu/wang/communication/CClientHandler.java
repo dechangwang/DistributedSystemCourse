e27b10c4a9d33acb568ea1fff529329317d825d8
dechangwang <13changice@tongji.edu.cn>
2018-01-07 16:01:28 +0800

    test：Client communicate with Server

---
+++ src/main/java/cn/edu/wang/communication/CClientHandler.java
@@ -0,0 +1,48 @@
+package cn.edu.wang.communication;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by wangdechang on 2018/1/7
+ */
+public class CClientHandler extends ChannelInboundHandlerAdapter  {
+    private static Logger logger = LoggerFactory.getLogger(CClientHandler.class);
+
+    // 接收server端的消息，并打印出来
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        logger.info("HelloClientIntHandler.channelRead");
+        ByteBuf result = (ByteBuf) msg;
+        byte[] result1 = new byte[result.readableBytes()];
+        result.readBytes(result1);
+        System.out.println("Server said:" + new String(result1));
+        result.release();
+        String msg1 = "Are you ok?";
+        ByteBuf encoded = ctx.alloc().buffer(4 * msg1.length());
+        encoded.writeBytes(msg1.getBytes());
+        ctx.write(encoded);
+        ctx.flush();
+    }
+
+    // 连接成功后，向server发送消息
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        logger.info("HelloClientIntHandler.channelActive");
+        String msg = "Are you ok?";
+        ByteBuf encoded = ctx.alloc().buffer(4 * msg.length());
+        encoded.writeBytes(msg.getBytes());
+        ctx.write(encoded);
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+        ctx.close();
+    }
+}
---
+++ src/main/java/cn/edu/wang/communication/CommunicateServer.java
@@ -0,0 +1,45 @@
+package cn.edu.wang.communication;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * Created by wangdechang on 2018/1/7
+ */
+public class CommunicateServer {
+    public void start(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch)
+                                throws Exception {
+                            // 注册handler
+                            ch.pipeline().addLast(new CServerHandler());
+                        }
+                    }).option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture f = b.bind(port).sync();
+
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        CommunicateServer server = new CommunicateServer();
+        server.start(8000);
+    }
+}
